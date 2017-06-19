273d69a543a814062587fac762cea745eb272299
baijie <baijie@meitunmama.com>
2017-06-20 06:56:32 +0800

    嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import userConfig from '@/page/base/user.config';
 
 Vue.use(Router);
 
@@ -10,6 +11,14 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+      children: [
+        {
+          // 当 /user/:id/profile 匹配成功，
+          // UserProfile 会被渲染在 User 的 <router-view> 中
+          path: 'userconfig',
+          component: userConfig,
+        },
+      ],
     },
   ],
 });
